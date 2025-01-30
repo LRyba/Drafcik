e9101ba2740f3268eead76c58981e460ec0036d9
LRRetoura <rybak@retoura.de>
2025-01-30 13:45:24 +0100

    refactoring

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,1 +1,53 @@
-export { }
+import { Stack, Typography, TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import { useState, useEffect } from "react";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { Round, Player } from "../model/model";
+import { roundsState } from "../states/RoundsState";
+import { playersState } from "../states/PlayerState";
+
+export const Leaderboard = () => {
+    const [players, setPlayers] = useRecoilState(playersState);
+    const matches = useRecoilValue(roundsState);
+    const [leaderboard, setLeaderboard] = useState<Player[]>([]);
+
+    // useEffect(() => {
+    //     const updatedPlayers = players.map((player) => {
+    //         if (player.matchIds.length !== 0) return player;
+    //         const playerMatches = matches.filter((match) => match.player1.name === player.name || match.player2.name === player.name);
+    //         return { ...player, matchIds: playerMatches.map((match) => match.id) };
+    //     });
+
+    //     setPlayers(updatedPlayers);
+    // }, [matches]);
+
+    // useEffect(() => {
+    //     const updatedLeaderboard = players.sort((a, b) => b.points - a.points);
+    //     setLeaderboard(updatedLeaderboard);
+    // }, [players]);
+
+    return (
+        <Stack spacing={3}>
+            <Typography variant='h4'>Tabela wyników</Typography>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Pozycja</TableCell>
+                            <TableCell>Zawodnik</TableCell>
+                            <TableCell>Punkty</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {leaderboard.map((player, index) => (
+                            <TableRow key={player.name}>
+                                <TableCell>{index + 1}</TableCell>
+                                <TableCell>{player.name}</TableCell>
+                                <TableCell>{player.points}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Stack>
+    );
+}
--- src/components/TournamentBracket.tsx
+++ src/components/TournamentBracket.tsx
@@ -1,87 +1,19 @@
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { playersState } from '../states/PlayerState';
-import { matchesState } from '../states/MatchesState';
+import { roundsState } from '../states/RoundsState';
 import { Match, Player, Round } from '../model/model';
 import { Button, Stack, styled } from '@mui/material';
 import { MatchElement } from './MatchElement';
 
-const robin = require('roundrobin');
+export interface TournamentBracketProps {
+  rounds: Round[];
+  onScoreUpdate: (matchId: string, score: Match['score']) => void;
+}
 
-export const TournamentBracket = () => {
+export const TournamentBracket = (props: TournamentBracketProps) => {
+  const { rounds, onScoreUpdate } = props;
   const [players, setPlayers] = useRecoilState(playersState);
-  const [matches, setMatches] = useRecoilState(matchesState);
-  const [rounds, setRounds] = useState<Round[]>([]);
-
-  console.log(players)
-
-  const generateTournamentRounds = (players: Player[]): Round[] => {
-    const playerNames = players.map((player) => player.name);
-    const robinOutput = robin(playerNames.length, playerNames);
-    let matchId = 0;
-
-    const generatedRounds: Round[] = robinOutput.map((round: string[][], roundIndex: any) => {
-      const unmatchedPlayers: Player[] = [...players];
-      const roundMatches: Match[] = round.map((matchPair: string[]) => {
-        const player1 = players.find((player) => player.name === matchPair[0]) as Player;
-        const player2 = players.find((player) => player.name === matchPair[1]) as Player;
-
-        unmatchedPlayers.splice(unmatchedPlayers.indexOf(player1), 1);
-        unmatchedPlayers.splice(unmatchedPlayers.indexOf(player2), 1);
-
-        const match = {
-          id: matchId++,
-          player1: player1,
-          player2: player2,
-          score: undefined,
-        };
-        return match;
-      });
-
-      return {
-        matches: roundMatches,
-        waitingPlayer: unmatchedPlayers[0] || undefined,
-      };
-    });
-
-    const allMatches = generatedRounds.flatMap((round) => round.matches);
-    setMatches(allMatches);
-
-    return generatedRounds;
-  };
-
-  const updatePlayersWithMatches = () => {
-    const updatedPlayers = players.map((player) => {
-      if (player.matchIds.length !== 0) return player;
-      const playerMatches = matches.filter((match) => match.player1.name === player.name || match.player2.name === player.name);
-      return { ...player, matchIds: playerMatches.map((match) => match.id) };
-    });
-
-    setPlayers(updatedPlayers);
-  };
-
-  const handleUpdateScore = (matchId: string, score: Match['score']) => {
-    // const updatedMatches = matches.map((match) => (match.id.toString() === matchId ? { ...match, score } : match));
-    // setMatches(updatedMatches);
-    const updatedRounds = rounds.map((round) => ({
-      ...round,
-      matches: round.matches.map((match) => (match.id.toString() === matchId ? { ...match, score } : match)),
-    }));
-    setRounds(updatedRounds);
-  };
-
-  useEffect(() => {
-    if (rounds.length === 0) {
-      const generatedRounds = generateTournamentRounds(players);
-      setRounds(generatedRounds);
-      const allMatches = generatedRounds.flatMap((round) => round.matches);
-      setMatches(allMatches);
-    }
-  }, [players, setMatches]);
-
-  useEffect(() => {
-    updatePlayersWithMatches();
-  }, [rounds]);
 
   const roundStyle = {
     display: 'flex',
@@ -102,7 +34,7 @@ export const TournamentBracket = () => {
               <MatchElement
                 key={`match-${match.id}`}
                 match={match}
-                onUpdateScore={(score) => handleUpdateScore(match.id.toString(), score)}
+                onUpdateScore={(score) => onScoreUpdate(match.id.toString(), score)}
               />
             ))}
             {round.waitingPlayer && <h3>Przerwa: {round.waitingPlayer.name}</h3>}
